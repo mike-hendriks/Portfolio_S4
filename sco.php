0e8ec3febc8f0a34e829fb7733558976a489d772
Mike Hendriks <mike@oddesseysolutions.nl>
2019-06-02 00:16:55 +0200

    Sco content added

--- sco.php
+++ sco.php
@@ -8,9 +8,142 @@
         </div>
     </div>
 
+    <!-- Conceptueel denken -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Conceptueel denken</h2>
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            De student toont aan divergentie- en convergentie technieken gebruikt te hebben om te komen tot het creatieve concept en heeft gedegen onderzoek gedaan naar doelgroep en de haalbaarheid van concepten besproken met een relevante expert. De student heeft minimaal vijf ideeën ontwikkeld om te komen tot het finale concept en laat dit zowel in woord als beeld zien. Het concept beantwoordt de klantvraag en is geëvalueerd met de doelgroep.
+                            <td>
+                            4.0 pts
+                            <br><br>
+                            De student toont aan divergentie- en convergentie technieken gebruikt te hebben om te komen tot het creatieve concept. De student heeft minimaal twee ideeën ontwikkeld om te komen tot het finale concept en heeft deze concepten getoetst met de doelgroep. Het concept is overtuigend gevisualiseerd en het beantwoordt de klantvraag.
+                            </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
 
+                <p>
+                    Gedurende gate 1 & 2 zijn wij constant bezig geweest met het verbeteren van ons concept. Zo hebben wij vele iteraties gehad over ons concept. We hebben het over verschillende ideeën gehad zoals een energiemonitor, een community, reminders voor medicatie en oefeningen, het delen van ervaringen, en nog veel meer.
+                    <br><br>
+                    Om tot deze ideeën te komen hebben wij verschillende technieken gebruikt. De voornaamste techniek die wij hebben gebruikt is mind mapping. Ook zijn we gaan kijken wat de patiënten echt nodig hebben. Door meerdere interviews te houden kwamen we achter veel frustraties, hier hebben wij dan weer ideeën aan vast gekoppeld waarmee wij hen mogelijk kunnen helpen.
+                </p>
+            </div>
 
-    <h1>SCO content here</h1>
+            <!-- Image slider block with text -->
+            <div class="col m6">
+                <p>
+                    Tijdens de workshop van anke heb ik samen met Max en Bart gekeken naar hoe parkinson patiënten mogelijk moeite kunnen hebben met het gebruik maken van mobiele telefoons en apps. Door een paper prototype te maken en deze te testen met mensen zonder parkinson, maar door hun armen op willekeurige momenten te laten schudden hebben wij geprobeerd een soort parkinson te simuleren. Hierdoor zijn we er achter gekomen dat kleine standaard knoppen zoals een “floating action button” in android te klein zijn en dat we hier rekening mee moeten houden tijdens het ontwerpen. Natuurlijk is dit geen onderzoek hoe je dit normaal uit zou voeren, maar aangezien er geen parkinson patiënt was om het mee te testen hebben we het op deze manier opgelost. Natuurlijk ga ik toekomstige prototypes en ontwerpen wel testen op parkinson patiënten.
+                </p>
+            </div>
+            <div class="col m6">
+                <div class="carousel owl-carousel">
+                        <div class="carousel owl-carousel">
+                            <img src="./public/images/workshop_anke.jpg" alt="" class="img-responsive">
+                            <img src="./public/images/workshop_anke_2.jpg" alt="" class="img-responsive">
+                        </div>
+                </div>
+            </div>
+
+            <!-- Text block -->
+            <div class="col s12">
+                <p>
+                    <b>Bezoek Parkinson Café - 11 april</b> <br>
+                    Tijdens het bezoek aan het Parkinson Café in eindhoven op 11 april heb ik, net zoals iedereen uit de proftaak groep, <a href="https://xd.adobe.com/view/f1faff5a-64dc-400a-47e7-befa34913d4b-87cb/?fullscreen" target="_blank">Prototype gate 2</a> getest op de doelgroep. Hierbij heb ik kort vooraf wat vragen gesteld over de gebruiker, mijzelf voorgesteld en verteld waar ons product voor dient. Doordat ik van te voren heb nagedacht over hoe ik het ging aanpakken merkte ik wel degelijk verschil met voorgaande tests uit andere semesters. Ik kreeg veel gerichter feedback op het product. Feedback waar wij ook duidelijk iets mee kunnen. In totaal is er bij het cafe getest met ongeveer 6 personen. De verwerkte resultaten zijn <a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">hier</a> te vinden.
+                    <br><br>
+
+                    <b>Ontwerpen en onderbouwing</b><br>
+                    Tijdens het project heb ik het een en ander ontworpen en bedacht. Meer informatie hierover is te vinden in het document <a href="https://docs.google.com/document/d/1oN1I2wTQHwd_WguXDZICuoJKO8jnxf0erBPPcoLVwAU/edit?usp=sharing" target="_blank">Interface onderbouwing</a>
+                </p>
+                <br>
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://xd.adobe.com/view/f1faff5a-64dc-400a-47e7-befa34913d4b-87cb/?fullscreen" target="_blank">Prototype gate 2</a></li>
+                    <li><a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">Parkinson Cafe usertest - 11 april</a></li>
+                    <li><a href="https://docs.google.com/document/d/1oN1I2wTQHwd_WguXDZICuoJKO8jnxf0erBPPcoLVwAU/edit?usp=sharing" target="_blank">Interface onderbouwing</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+
+    <!-- Onderbouwen van de media-inzet -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Onderbouwen van de media-inzet</h2>
+
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            De student kan de inzet van media geheel verantwoorden vanuit methodisch* onderzoek naar doelgroep en doelstellingen, is in lijn met het concept en beantwoordt de vraag van de opdrachtgever.                            
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student kan de inzet van media gedeeltelijk verantwoorden vanuit onderzoek naar doelgroep en doelstellingen. De klantvraag wordt gedeeltelijk, maar voldoende beantwoordt.
+                        </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+
+                <p>
+                </p>
+
+                <br>
+                <h4>Bewijslast</h4>
+                <!-- <ul>
+                    <li><a href="https://docs.google.com/document/d/11eJ1lPo7GzCOf09mF5phCBeJdROOIN2dCMS5FFvUw4Y/edit?usp=sharing" target="_blank">Scrum proces</a></li>
+                </ul> -->
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+
+    <!-- Executie -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Executie</h2>
+
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            De student heeft aantoonbaar gewerkt aan diverse prototypes kan minimaal drie iteraties tonen. De student heeft bij het ontwerp van de prototypes een bewuste keuze gemaakt uit verschillende alternatieven voor typografie, beeld en tekens, short copy en tone-of-voice en kan deze keuzes beargumenteren vanuit merk, doelgroep, doelen en concept.
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student heeft aantoonbaar gewerkt aan diverse prototypes en kan minimaal twee iteraties tonen. De student heeft een bewuste keuze gemaakt voor typografie, beeld en tekens, short copy en tone-of-voice en kan deze beargumenteren vanuit merk, doelgroep, doelstellingen en concept.
+                        </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+
+                <p>
+                </p>
+
+                <br>
+                <h4>Bewijslast</h4>
+                <!-- <ul>
+                    <li><a href="https://docs.google.com/document/d/11eJ1lPo7GzCOf09mF5phCBeJdROOIN2dCMS5FFvUw4Y/edit?usp=sharing" target="_blank">Scrum proces</a></li>
+                </ul> -->
+            </div>
+        </div>
+    </div>
+    <br><br><br>
 
 
 
