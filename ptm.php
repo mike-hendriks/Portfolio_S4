4641d98660989612e5de8726399be6a937aecdbf
Mike Hendriks <mike@oddesseysolutions.nl>
2019-06-02 00:02:43 +0200

    Proftaak content placed

--- ptm.php
+++ ptm.php
@@ -8,9 +8,204 @@
         </div>
     </div>
 
+    <!-- Project Methodiek -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Project Methodiek</h2>
+
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            De student heeft de controle over het ontwikkelproces en is in staat te reflecteren door kritisch te zijn naar zichzelf en medestudenten met betrekking tot het Scrum proces.
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student kan de Scrum begrippen uitleggen middels voorbeelden en user stories, laat duidelijk zien dat de toepassing van Scrum verbeterd is over de sprints en kan aantonen dat er actief en doelmatig is samengewerkt aan het mediaproduct.
+                        </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+
+                <p>
+                    Tijdens de afgelopen weken is mijn kennis in scrum enorm gestegen. Ik dacht hier al wat kennis in te hebben doordat ik het in semester 3 al heb gebruikt, maar dit was slechts een hele kleine greep van het grote geheel. 
+                    <br><br>
+                    In het document hieronder vind je al mijn bevindingen over scrum en hoe het mij de afgelopen weken is afgegaan.
+                </p>
+                <br>
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://docs.google.com/document/d/11eJ1lPo7GzCOf09mF5phCBeJdROOIN2dCMS5FFvUw4Y/edit?usp=sharing" target="_blank">Scrum proces</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <!-- Oordeelsvorming -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Oordeelsvorming</h2>
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            6.0 pts 
+                            De student kan voor alle leerlijnen het eigen werk methodisch en inhoudelijk verantwoorden met de onderzoekscyclus.
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student formuleert de juiste onderzoeks- en deelvragen, hanteert goede onderzoeksstrategieën en technieken toe om het eigen werk te verantwoorden.                        </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+
+                <p>
+                    Als groep hebben wij ons onderverdeeld in 3 deelvragen, samen met Bart hebben wij de deelvraag “Welke bestaande hulpmiddelen bestaan er al voor parkinson patiënten?” op ons genomen.
+                    <br><br>
+                    Om deze vraag te beantwoorden hebben wij de volgende onderzoeken gedaan: <br><br>
+                    <b>Benchmark creation | Bieb</b> <br>
+                    Door zelf onderzoek doen op het internet en in brochures van het ziekenhuis ben ik (samen met Bart) achter veel hulpmiddelen gekomen waar wij anders nooit op waren gekomen.
+                    Wij hebben voor deze methode gekozen omdat wij hiermee snel tot veel resultaten konden komen. <br><br>
+
+                    <b>Expert interview | Veld</b> <br>
+                    Om onze inzichten te vergroten en achter informatie te komen die wij zelf niet konden vinden op internet hebben wij er voor gekozen om een interview/ gesprek te houden met een parkinsonverpleegkundige.
+                    <br><br>
+                    <b>USP</b> <br>
+                    Door te kijken naar USP’s van andere producten maar ook naar die van ons kunnen wij potentieel de beste dingen uit andere producten halen om ons eigen concept te versterken of verbeteren.
+                    <br> <br>
+                    <b>Day in the life | Veld</b> <br>
+                    Het mini onderzoek “A day in the life” heeft mij een beter inzicht gegeven in hoe de tremors het dagelijkse leven kunnen beïnvloeden. Simpele taken zoals iets drinken kan al een grote opgave zijn. Als je bijvoorbeeld een onverwachte tremor krijgt tijdens het drinken kan het glas al van je mond los raken waardoor je drinken overal ligt. Dit soort simpele taken worden zo dus al veel moeilijker. Door dit onderzoek heb ik veel meer empathie gekregen voor parkinson patiënten.
+                    <br><br>
+                    Na afloop van de tweede sprint ben ik er van overtuigd dat wij de juiste methodes hebben gekozen om deze vraag te beantwoorden. Door de combinatie van bieb en veld onderzoek zijn wij tot inzichten gekomen waar wij anders niet op waren gekomen.
+
+                    <br><br>
+                    Tijdens ons gesprek met een parkinsonverpleegkundige hebben we veel inzicht gekregen over wat wel en niet werkt bij parkinson patiënten. Zo hadden wij als groep eerst een idee om de patiënten te assisteren met het initiatief te nemen door hun energie te meten en op basis hiervan suggesties te doen voor activiteiten zoals wandelingen. Echter vertelde de verpleegkundige ons dat dit helemaal niet zou kunnen werken. De patiënten hebben echt iemand nodig die hun mee neemt, een simpele notificatie gaat hierbij niet helpen.
+                </p>
+                <br>
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://docs.google.com/document/d/1vWBSlOnRip88YsMuzoMdp6u-qB5gzLuMiRMHghgpkxg/edit?usp=sharing" target="_blank">Onderzoeksdocument</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=EYMnHK3H6vQ&feature=youtu.be" target="_blank">A day in the life</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <br><br><br>
+    <!-- Leervaardigheden -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Leervaardigheden</h2>
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            6.0 pts 
+                            De student betrekt zijn zelfreflectie en ontvangen feedback op het eigen functioneren in het ICT vakgebied en beschrijft in zijn visie zowel zijn talenten als ontwikkelings ambities.
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student beschrijft zijn professionele talenten en ontwikkeling ambities binnen het ICT vakgebied en geeft middels zelfreflectie en feedbackmomenten aan hoe hij/zij hier aan heeft gewerkt.
+                           </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+
+                <p>
+                    De door mijzelf opgezette leerdoelen zijn:
+                    <br><br>
+                    Om deze vraag te beantwoorden hebben wij de volgende onderzoeken gedaan: <br><br>
+                    <b>Oordeelsvorming</b> <br>
+                    Tijdens dit project wil ik beter leren omgaan triangulatie. Ook wil ik beter in staat zijn om relevante onderzoeksmethoden te kiezen voor de juiste projecten/ onderzoeken.
+                    <br><br>
+
+                    <b>Communicatie</b> <br>
+                    Tijdens gebruikersonderzoeken wil ik beter in staat zijn het concept over te brengen zonder hier al te veel over te vertellen. Tijdens eerdere gebruikersonderzoeken had ik het gevoel dat ik alles te diep uitleg waardoor de antwoorden kunnen worden beïnvloed.
+                    <br><br>
+                    
+                    <b>Leervaardigheden</b> <br>
+                    Tijdens semester 4 wil ik gebruik leren maken van React Native. Hierbij hoop ik ook nog extra kennis en oefening op te doen in React zelf. Buiten programmeren wil ik ook beter leren omgaan met het DOT framework, dit sluit weer aan het trianguleren uit het leerdoel van oordeelsvorming.
+                    <br> <br>
+
+                    Door de juiste onderzoeksmethodes te kiezen ben ik (samen met Bart) in staat geweest om veel nieuwe inzichten te krijgen over de hulpmiddelen voor parkinson patiënten, maar ook over wat er wel en niet mogelijk is in hun leven door de ziekte.
+
+                    <br><br>
+
+                    <b>Bezoek Parkinson Café - 11 april</b> <br>
+                    Tijdens het bezoek aan het Parkinson Café in eindhoven op 11 april heb ik, net zoals iedereen uit de proftaak groep, <a href="https://xd.adobe.com/view/f1faff5a-64dc-400a-47e7-befa34913d4b-87cb/?fullscreen" target="_blank">Prototype gate 2</a> getest op de doelgroep. Hierbij heb ik kort vooraf wat vragen gesteld over de gebruiker, mijzelf voorgesteld en verteld waar ons product voor dient. Doordat ik van te voren heb nagedacht over hoe ik het ging aanpakken merkte ik wel degelijk verschil met voorgaande tests uit andere semesters. Ik kreeg veel gerichter feedback op het product. Feedback waar wij ook duidelijk iets mee kunnen. In totaal is er bij het cafe getest met ongeveer 6 personen. De verwerkte resultaten zijn <a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">hier</a> te vinden.
+                    <br><br>
+
+                    <b>React</b> <br>
+                    Ik vind het heel fijn om met React te werken, reusability is een groot aspect van React en ik ben hier een grote voorstander van. Als programmeur wil ik zo min mogelijk herhalend werk doen en React geeft hier een uitstekende basis voor. Uiteraard is Javascript ook een hele fijne programmeertaal.
+                    <br><br>
+                    Ondanks dat ik nog veel te leren heb over React, en Javascript in het algemeen ben ik er van overtuigd dat ik in de paar weken dat ik geprogrammeerd heb al gegroeid ben. Zo heb ik een beter idee over hoe je componenten echt goed kan hergebruiken en wil ik mij nu gaan focussen op de werking van Redux.
+                </p>
+                <br>
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://docs.google.com/document/d/11eJ1lPo7GzCOf09mF5phCBeJdROOIN2dCMS5FFvUw4Y/edit?usp=sharing" target="_blank">Scrum Proces</a></li>
+                    <li><a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">Parkinson Cafe usertest - 11 april</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <br><br><br>
+    <!-- Communicatie -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h2>Communicatie</h2>
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            6.0 pts 
+                            De student rapporteert middels correct taalgebruik, zorgt voor logisch gestructureerde documenten en hanteert bronvermelding middels APA-stijl. De student kan zijn werk op een overtuigende manier presenteren en er vragen over beantwoorden.                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student kan zijn werk op een heldere gestructureerde manier presenteren, zowel mondeling, tekstueel als visueel en hanteert bronvermelding.
+                           </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+
+                <p>
+                Aan het einde van gate 1 heb ik samen met Mark en Justin ons idee gepresenteerd. Dit ging erg goed, het idee was goed overgebracht en we kregen als feedback dat het een goede duidelijke presentatie was.
+                <br><br>
+                Voor de bronvermelding hebben Bart en ik gebruik gemaakt van de IPA stijl bronvermelding, dit vind je terug in ons onderzoeksdocument.
+                
+                <b>Bezoek Parkinson Café - 11 april</b> <br>
+                Tijdens het bezoek aan het Parkinson Café in eindhoven op 11 april heb ik, net zoals iedereen uit de proftaak groep, <a href="https://xd.adobe.com/view/f1faff5a-64dc-400a-47e7-befa34913d4b-87cb/?fullscreen" target="_blank">Prototype gate 2</a> getest op de doelgroep. Hierbij heb ik kort vooraf wat vragen gesteld over de gebruiker, mijzelf voorgesteld en verteld waar ons product voor dient. Doordat ik van te voren heb nagedacht over hoe ik het ging aanpakken merkte ik wel degelijk verschil met voorgaande tests uit andere semesters. Ik kreeg veel gerichter feedback op het product. Feedback waar wij ook duidelijk iets mee kunnen. In totaal is er bij het cafe getest met ongeveer 6 personen. De verwerkte resultaten zijn <a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">hier</a> te vinden.
+                <br><br>
 
 
-    <h1>PTM content here</h1>
+
+                </p>
+                <br>
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://docs.google.com/document/d/1vWBSlOnRip88YsMuzoMdp6u-qB5gzLuMiRMHghgpkxg/edit?usp=sharing" target="_blank">Onderzoeksdocument</a></li>
+                    <li><a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">Parkinson Cafe usertest - 11 april</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 
 
