91fe3b9469fe3177c935cf253052e26ce34ba19b
Mike Hendriks <mike@oddesseysolutions.nl>
2019-06-02 13:04:31 +0200

    Ux content placed

--- ux.php
+++ ux.php
@@ -8,9 +8,168 @@
         </div>
     </div>
 
+    <!-- Conceptueel denken -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+            <h2>Conceptueel denken</h2>
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            De student heeft in elke sprint gebruikersonderzoek gedaan waarbij hij diepgaande inzichten in de doelgroep heeft verkregen en hierover een eigen visie ontwikkeld heeft die gebruikt is bij het mediaproduct.
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student heeft bij het ontwikkelen van het mediaproduct methodisch gebruikersonderzoek uitgevoerd en daarnaast ook via literatuuronderzoek inzichten in de doelgroep verkregen, die toegepast zijn in het mediaproduct.
+                        </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
 
+                <p>
+                    Als groep hebben wij ons onderverdeeld in 3 deelvragen, samen met Bart hebben wij de deelvraag “Welke bestaande hulpmiddelen bestaan er al voor parkinson patiënten?” op ons genomen.
+                    <br><br>
+                    Om deze vraag te beantwoorden hebben wij de volgende onderzoeken gedaan:
+                    <br><br>
+                    <b>Benchmark creation | Bieb</b><br>
+                    Door zelf onderzoek doen op het internet en in brochures van het ziekenhuis ben ik (samen met Bart) achter veel hulpmiddelen gekomen waar wij anders nooit op waren gekomen.
+                    Wij hebben voor deze methode gekozen omdat wij hiermee snel tot veel resultaten konden komen.
+                    <br><br>
 
-    <h1>UX content here</h1>
+                    <b>Expoert interview | Veld</b><br>
+                    Om onze inzichten te vergroten en achter informatie te komen die wij zelf niet konden vinden op internet hebben wij er voor gekozen om een interview/ gesprek te houden met een parkinsonverpleegkundige.
+                    <br><br>
+
+                    <b>USP</b><br>
+                    Door te kijken naar USP’s van andere producten maar ook naar die van ons kunnen wij potentieel de beste dingen uit andere producten halen om ons eigen concept te versterken of verbeteren.
+                    <br><br>
+
+                    <b>Day in the life| Veld</b><br>
+                    Het mini onderzoek “A day in the life” heeft mij een beter inzicht gegeven in hoe de tremors het dagelijkse leven kunnen beïnvloeden. Simpele taken zoals iets drinken kan al een grote opgave zijn. Als je bijvoorbeeld een onverwachte tremor krijgt tijdens het drinken kan het glas al van je mond los raken waardoor je drinken overal ligt. Dit soort simpele taken worden zo dus al veel moeilijker. Door dit onderzoek heb ik veel meer empathie gekregen voor parkinson patiënten.
+                    <br><br>
+
+                    Na afloop van de tweede sprint ben ik er van overtuigd dat wij de juiste methodes hebben gekozen om deze vraag te beantwoorden. Door de combinatie van bieb en veld onderzoek zijn wij tot inzichten gekomen waar wij anders niet op waren gekomen.
+                    <br><br>
+
+                    Tijdens ons gesprek met een parkinsonverpleegkundige hebben we veel inzicht gekregen over wat wel en niet werkt bij parkinson patiënten. Zo hadden wij als groep eerst een idee om de patiënten te assisteren met het initiatief te nemen door hun energie te meten en op basis hiervan suggesties te doen voor activiteiten zoals wandelingen. Echter vertelde de verpleegkundige ons dat dit helemaal niet zou kunnen werken. De patiënten hebben echt iemand nodig die hun mee neemt, een simpele notificatie gaat hierbij niet helpen.
+                    <br><br>
+
+                    <b>Bezoek Parkinson Café - 11 april</b><br>
+                    Tijdens het bezoek aan het Parkinson Café in eindhoven op 11 april heb ik, net zoals iedereen uit de proftaak groep, <a href="https://xd.adobe.com/view/f1faff5a-64dc-400a-47e7-befa34913d4b-87cb/?fullscreen" target="_blank">Prototype gate 2</a> getest op de doelgroep. Hierbij heb ik kort vooraf wat vragen gesteld over de gebruiker, mijzelf voorgesteld en verteld waar ons product voor dient. Doordat ik van te voren heb nagedacht over hoe ik het ging aanpakken merkte ik wel degelijk verschil met voorgaande tests uit andere semesters. Ik kreeg veel gerichter feedback op het product. Feedback waar wij ook duidelijk iets mee kunnen. In totaal is er bij het cafe getest met ongeveer 6 personen. De verwerkte resultaten zijn <a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">hier</a> te vinden.
+                </p>
+                <br>
+
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://docs.google.com/document/d/1vWBSlOnRip88YsMuzoMdp6u-qB5gzLuMiRMHghgpkxg/edit?usp=sharing" target="_blank">Onderzoeksdocument</a></li>
+                    <li><a href="https://www.youtube.com/watch?v=EYMnHK3H6vQ&feature=youtu.be" target="_blank">A day in the life</a></li>
+                    <li><a href="https://docs.google.com/document/d/15PQBcwdE32wnlBglgcy5TTSkAmiFFcCCwc10EoTu99E/edit?usp=sharing" target="_blank">Parkinson Cafe usertest - 11 april</a></li>
+                </ul>
+            </div>    
+        </div>
+    </div>
+    <br><br><br>
+
+    
+    <!-- Prototype voorleggen -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+            <h2>Prototype voorleggen</h2>
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            In het ontwikkelproces heeft de student diverse prototypen gemaakt van innovatieve, verrassende producten die dmv gebruikersonderzoek inzichten opleveren voor het eindproduct.
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            In het ontwikkelproces heeft de student diverse prototypen gemaakt die methodisch getest zijn met gebruikers en inzichten opleveren voor het eindproduct. Dit leidt tot minimaal twee iteraties op het prototype.
+                        </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s6">
+                <p>
+                    Tijdens de workshop van Anke heb ik een prototype gemaakt om kleine functies in de app en de interactie tussen iemand met (nagebootste) tremors en een mobiel te testen. Dit heb ik getest met een aantal andere leerlingen en hierop heb ik samen met Max en Bart gereflecteerd.
+                    <br><br>
+                    Ook ben ik bezig geweest met het eerste prototype dat wij hebben getoont bij de demo in gate 2. Ik heb voornamelijk iteraties gemaakt op het homescherm en hoe we de ruimte optimaal kunnen benutten zodat er geen overbodige acties uitgevoerd hoeven te worden.
+                </p>
+            </div>
+            <div class="col s6">
+                <div class="carousel owl-carousel">
+                    <img src="./public/images/workshop_anke.jpg" alt="" class="img-responsive">
+                    <img src="./public/images/workshop_anke_2.jpg" alt="" class="img-responsive">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12">
+                <img src="./public/images/design_process.png" alt="" class="img-responsive">
+                <p>
+                    <br>
+                    <b>Ontwerpen en onderbouwing</b><br>
+                    Tijdens het project heb ik het een en ander ontworpen en bedacht. Meer informatie hierover is te vinden in het document <a href="https://docs.google.com/document/d/1oN1I2wTQHwd_WguXDZICuoJKO8jnxf0erBPPcoLVwAU/edit?usp=sharing" target="_blank">Interface onderbouwing</a>
+                </p>
+            </div>
+            <br>
+            <div class="col s12">
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://docs.google.com/document/d/1oN1I2wTQHwd_WguXDZICuoJKO8jnxf0erBPPcoLVwAU/edit?usp=sharing" target="_blank">Interface onderbouwing</a></li>
+                    <li><a href="https://xd.adobe.com/view/f1faff5a-64dc-400a-47e7-befa34913d4b-87cb/?fullscreen" target="_blank">Prototype gate 2</a></li>
+                </ul>
+            </div>    
+        </div>
+    </div>
+    <br><br><br>
+    <!-- Interface onderbouwing -->
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+            <h2>Interface onderbouwing</h2>
+                <table class="four_columns">
+                    <tr>
+                        <td>10.0 pts</td>
+                        <td>
+                            6.0 pts
+                            <br><br>
+                            De student heeft de interface van het mediaproduct gebaseerd op een door hem ontwikkelde set van design patterns en aangepast.
+                        </td>
+                        <td>
+                            4.0 pts
+                            <br><br>
+                            De student heeft de interface van het mediaproduct getoetst aan een door hem gekozen set design guidelines en op basis daarvan het interface ontwerp verbeterd.
+                        </td>
+                        <td>0.0 pts</td>
+                    </tr>
+                </table>
+                
+                <p>
+                    <b>Ontwerpen en onderbouwing</b><br>
+                    Tijdens het project heb ik het een en ander ontworpen en bedacht. Meer informatie hierover is te vinden in het document <a href="https://docs.google.com/document/d/1oN1I2wTQHwd_WguXDZICuoJKO8jnxf0erBPPcoLVwAU/edit?usp=sharing" target="_blank">Interface onderbouwing</a>
+                </p>
+
+                <br>
+                <h4>Bewijslast</h4>
+                <ul>
+                    <li><a href="https://docs.google.com/document/d/1oN1I2wTQHwd_WguXDZICuoJKO8jnxf0erBPPcoLVwAU/edit?usp=sharing" target="_blank">Interface onderbouwing</a></li>
+                    <li><a href="https://xd.adobe.com/view/f1faff5a-64dc-400a-47e7-befa34913d4b-87cb/?fullscreen" target="_blank">Prototype gate 2</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
 
 
 
